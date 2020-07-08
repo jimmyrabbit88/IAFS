cc1ba9fbc1d71316eb4bd9df5163fdad9387f633
Jason <jason.dowling@students.ittralee.ie>
2020-07-08 07:39:37 +0100

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,7 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: "AIzaSyB8yti03zK-LBtwof35q5bys_Gm2EYMQw4",
-    authDomain: "iafs-fbf21.firebaseapp.com",
-    databaseURL: "https://iafs-fbf21.firebaseio.com",
-    projectId: "iafs-fbf21",
-    storageBucket: "iafs-fbf21.appspot.com",
-    messagingSenderId: "296333589028",
-    appId: "1:296333589028:web:095a1da0e549bf13ad76b1",
-    measurementId: "G-KYY8TW4PH0"
-  }
+  //testing
 };
 
 /*
